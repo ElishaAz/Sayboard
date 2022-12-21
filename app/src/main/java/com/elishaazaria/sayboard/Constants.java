98ab70a3cb3ea74abb4b2ef35f821b421a208895
Elisha Azaria <elishadar@gmail.com>
2022-12-21 13:19:41 +0200

    Moved models to external storage

--- app/src/main/java/com/elishaazaria/sayboard/Constants.java
+++ app/src/main/java/com/elishaazaria/sayboard/Constants.java
@@ -1,6 +1,7 @@
 package com.elishaazaria.sayboard;
 
 import android.content.Context;
+import android.os.Environment;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -10,17 +11,33 @@ import java.util.Locale;
 public class Constants {
     public static String DOWNLOADER_CHANNEL_ID = "downloader";
 
+    private static File getCacheDir(Context context) {
+        if (Environment.isExternalStorageEmulated() || !Environment.isExternalStorageRemovable()) {
+            return context.getExternalCacheDir();
+        } else {
+            return context.getCacheDir();
+        }
+    }
+
+    private static File getFilesDir(Context context) {
+        if (Environment.isExternalStorageEmulated() || !Environment.isExternalStorageRemovable()) {
+            return context.getExternalFilesDir(null);
+        } else {
+            return context.getFilesDir();
+        }
+    }
+
     public static File getTemporaryDownloadLocation(Context context, String filename) {
-        File dir = new File(context.getCacheDir().getAbsolutePath(), "ModelZips");
+        File dir = new File(getCacheDir(context).getAbsolutePath(), "ModelZips");
         return new File(dir, filename);
     }
 
     public static File getTemporaryUnzipLocation(Context context) {
-        return new File(context.getCacheDir(), "TempUnzip");
+        return new File(getCacheDir(context), "TempUnzip");
     }
 
     public static File getModelsDirectory(Context context) {
-        return new File(context.getFilesDir().getAbsolutePath(), "Models");
+        return new File(getFilesDir(context).getAbsolutePath(), "Models");
     }
 
     public static File getDirectoryForModel(Context context, Locale locale) {
