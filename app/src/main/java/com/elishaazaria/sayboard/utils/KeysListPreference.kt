c7e5ee58cd37c0a55eda8351eed0712e47077283
Elisha Azaria <elishadar@gmail.com>
2023-11-08 18:36:40 +0200

    Fixed a bug with android replacing encoded newline character. Closes #49

--- app/src/main/java/com/elishaazaria/sayboard/utils/KeysListPreference.kt
+++ app/src/main/java/com/elishaazaria/sayboard/utils/KeysListPreference.kt
@@ -1,7 +1,7 @@
 package com.elishaazaria.sayboard.utils
 
 import dev.patrickgold.jetpref.datastore.model.PreferenceSerializer
-import kotlinx.serialization.*
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 
@@ -11,7 +11,8 @@ data class Key(val label: String, val text: String)
 class KeysListSerializer : PreferenceSerializer<List<Key>> {
     private val serializer = ListSerializer(Key.serializer())
     override fun deserialize(value: String): List<Key> {
-        return Json.decodeFromString(serializer, value)
+        // Android replaces literal "\n" into \ + newline for some reason
+        return Json.decodeFromString(serializer, value.replace("\\\n", "\\n"))
     }
 
     override fun serialize(value: List<Key>): String {
